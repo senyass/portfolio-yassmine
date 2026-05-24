8a8cf86affce029ca191d1eb664bf302d88c0932
yassou <senyass02@gmail.com>
2026-05-24 13:58:46 -0400

    Fixed small project card layout issue

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -19,7 +19,7 @@ function Projects() {
         Projects
       </motion.h1>
 
-<div className="relative max-w-[1400px] mx-auto grid grid-cols-1 xl:grid-cols-2 gap-y-20 xl:gap-y-32 gap-x-30">
+<div className="relative max-w-[1400px] mx-auto grid grid-cols-1 xl:grid-cols-2 gap-y-20 xl:gap-y-32 gap-x-30 items-start">
 
         <motion.img initial={{ opacity: 0, y: 40 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.7 }} viewport={{ once: false, amount: 0.3 }} src={flowerDarkPink} alt="Flower" className="absolute -top-50 -right-25 min-[500px]:-right-35 min-[500px]:-top-60 min-[1770px]:-right-65 min-[1770px]:-top-60 w-[160px] min-[500px]:w-[220px] rotate-30"/>
         <motion.img initial={{ opacity: 0, x: -40 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.7 }} viewport={{ once: false, amount: 0.3 }} src={flowerDarkPink} alt="Flower" className="absolute -top-45 -right-5 min-[500px]:-right-10 min-[500px]:-top-50 min-[1770px]:-right-35 min-[1770px]:-top-50 w-[120px] min-[500px]:w-[150px] rotate-320"/>
